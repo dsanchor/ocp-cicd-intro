1c3b79e50e8fa3d5697a73e919286ac8a20b7767
dsanchor <dsanchor@redhat.com>
2021-02-09 22:44:49 +0100

    Renamed certain vars

--- bootstrap/bootstrap-user.sh
+++ bootstrap/bootstrap-user.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-USER=$1
-CICD_NS=$2
+CICD_NS=$1
+USER=$2
+USER_NS=$3
 
 echo "Bootstraping user $USER (CI/CD ns=$CICD_NS)"
 
 echo "Creating namespaces..."
-cat ./ocp/ns/namespaces.yaml | USER=$USER envsubst | oc apply -f -
+cat ./ocp/ns/namespaces.yaml | USER_NS=$USER_NS envsubst | oc apply -f -
 
 echo "Create self-contained user group"
-cat ./ocp/groups/regular-user.yaml | USER=$USER envsubst | oc apply -f -
+cat ./ocp/groups/regular-user.yaml | USER=$USER USER_NS=$USER_NS envsubst | oc apply -f -
 
 echo "Adding view role to ns=$CICD_NS"
-cat ./ocp/rb/cicd-ns-rb.yaml | USER=$USER CICD_NS=$CICD_NS envsubst | oc apply -f -
+cat ./ocp/rb/cicd-ns-rb.yaml | USER=$USER CICD_NS=$CICD_NS USER_NS=$USER_NS envsubst | oc apply -f -
 
 echo "Adding admin role to its ns"
-cat ./ocp/rb/user-admin.yaml | USER=$USER envsubst | oc apply -f -
+cat ./ocp/rb/user-admin.yaml | USER=$USER USER_NS=$USER_NS envsubst | oc apply -f -
 
-echo "Adding system:image-puller from $USER-dev & $USER-prod ns to $USER ns"
-cat ./ocp/rb/user-ns-image-puller.yaml| USER=$USER envsubst | oc apply -f -
+echo "Adding system:image-puller from $USER_NS-dev & $USER_NS-prod ns to $USER_NS ns"
+cat ./ocp/rb/user-ns-image-puller.yaml| USER_NS=$USER_NS envsubst | oc apply -f -
 
-echo "Creating ArgoCD AppProject for user ${USER}"
-cat ./argocd/app-project.yaml | USER=$USER CICD_NS=$CICD_NS envsubst | oc apply -f -
+echo "Creating ArgoCD AppProject for user ${USER_NS}"
+cat ./argocd/app-project.yaml | USER_NS=$USER_NS CICD_NS=$CICD_NS envsubst | oc apply -f -
 
-echo "Adding ArgoCD Apps edit role for user ${USER}"
-cat ./ocp/rb/argocd-apps-editor.yaml | USER=$USER CICD_NS=$CICD_NS envsubst | oc apply -f -
+echo "Adding ArgoCD Apps edit role for user ${USER_NS}"
+cat ./ocp/rb/argocd-apps-editor.yaml | USER_NS=$USER_NS CICD_NS=$CICD_NS envsubst | oc apply -f -
