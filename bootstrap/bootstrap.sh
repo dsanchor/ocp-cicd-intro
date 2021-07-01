9eeb8b2faccde87d5b1686b3b16394e5c395d112
David Sancho Ruiz <dsanchor@redhat.com>
2021-07-01 10:18:56 +0200

    Updated resources

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -3,32 +3,32 @@
 ADMIN_USER=$1
 CICD_NS=$2
 
-# echo "Creating ${CICD_NS} namespace"
+echo "Creating ${CICD_NS} namespace"
 
-# oc new-project $CICD_NS
+oc new-project $CICD_NS
 
-# echo "Creating Nexus"
+echo "Creating Nexus"
 
-# oc apply -f ./nexus -n $CICD_NS
+oc apply -f ./nexus -n $CICD_NS
 
-# echo "Bootstraping Pipelines"
+echo "Bootstraping Pipelines"
 
-# echo "Creating operator subscription"
-# cat ./pipelines/subscription.yaml | oc apply -f -
+echo "Creating operator subscription"
+cat ./pipelines/subscription.yaml | oc apply -f -
 
-# echo "Bootstraping ArgoCD"
+echo "Bootstraping ArgoCD"
 
-# echo "Creating operator subscription"
-# # cat ./argocd/subscription.yaml | CICD_NS=$CICD_NS envsubst | oc apply -f -
-# cat ./argocd/subscription.yaml | oc apply -f -
+echo "Creating operator subscription"
+# cat ./argocd/subscription.yaml | CICD_NS=$CICD_NS envsubst | oc apply -f -
+cat ./argocd/subscription.yaml | oc apply -f -
 
-# while [ `oc get pods -n openshift-operators | grep gitops-operator | grep "1/1" | wc -l` -eq 0 ]
-# # while [ `oc get pods -n ${CICD_NS} | grep argocd-operator | grep "1/1" | wc -l` -eq 0 ]
-# do
-#   echo "Waiting for ArgoCD operator to be installed"
-#   sleep 10
-# done
-# echo "ArgoCD operator installed succesfully"
+while [ `oc get pods -n openshift-operators | grep gitops-operator | grep "1/1" | wc -l` -eq 0 ]
+# while [ `oc get pods -n ${CICD_NS} | grep argocd-operator | grep "1/1" | wc -l` -eq 0 ]
+do
+  echo "Waiting for ArgoCD operator to be installed"
+  sleep 10
+done
+echo "ArgoCD operator installed succesfully"
 
 echo "Install ArgoCD"
 oc apply -f ./argocd/argocd.yaml -n $CICD_NS
@@ -39,6 +39,6 @@ cat ./argocd/controller-rb.yaml  | CICD_NS=$CICD_NS envsubst | oc apply -f -
 echo "Create argocd-manager group"
 cat ./ocp/groups/argocd-manager.yaml | ADMIN_USER=$ADMIN_USER envsubst | oc apply -f -
 
-# echo "Installing SonarQube"
+echo "Installing SonarQube"
 
-# oc process -f sonarqube/sonarqube-template.yaml | oc apply -f - -n $CICD_NS
+oc process -f sonarqube/sonarqube-template.yaml | oc apply -f - -n $CICD_NS
